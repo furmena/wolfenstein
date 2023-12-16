20717ad011fceace966946ca9f1f35b2a283c8ec
Furmena <diaschithira21@gmail.com>
2023-12-16 18:45:06 +1100

    aurgh ive been so bored but finnaly i got to the point just before i do stuff with perp_wall_dist look at the lodev.org site if you dont know what im talking about

--- src/main.rs
+++ src/main.rs
@@ -3,10 +3,50 @@ use std::time::Duration;
 use sdl2::event::Event;
 use sdl2::keyboard::Keycode;
 
-const SCREEN_HEIGHT: u32 = 480;
-const SCREEN_WIDTH: u32 = 640;
-const MAP_HEIGHT: u32 = 24;
-const MAP_WIDTH: u32 = 24;
+const SCREEN_HEIGHT: i32 = 480;
+const SCREEN_WIDTH: i32 = 640;
+const MAP_HEIGHT: usize = 24;
+const MAP_WIDTH: usize = 24;
+
+// https://www.reddit.com/r/rust/comments/icpdvh/rust_matrix_structure/
+pub struct Vec2 {
+    x: f32,
+    y: f32
+}
+
+pub struct Matrix {
+    grid: [u32; MAP_WIDTH*MAP_HEIGHT]
+}
+
+const WORLD_MAP: Matrix = Matrix {
+    grid:   
+    [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,
+    1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,
+    1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,
+    1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,
+    1,0,0,0,0,0,2,2,2,2,2,0,0,0,0,3,0,3,0,3,0,0,0,1,
+    1,0,0,0,0,0,2,0,0,0,2,0,0,0,0,0,0,0,0,0,0,0,0,1,
+    1,0,0,0,0,0,2,0,0,0,2,0,0,0,0,3,0,0,0,3,0,0,0,1,
+    1,0,0,0,0,0,2,0,0,0,2,0,0,0,0,0,0,0,0,0,0,0,0,1,
+    1,0,0,0,0,0,2,2,0,2,2,0,0,0,0,3,0,3,0,3,0,0,0,1,
+    1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,
+    1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,
+    1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,
+    1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,
+    1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,
+    1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,
+    1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,
+    1,4,4,4,4,4,4,4,4,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,
+    1,4,0,4,0,0,0,0,4,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,
+    1,4,0,0,0,0,5,0,4,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,
+    1,4,0,4,0,0,0,0,4,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,
+    1,4,0,4,4,4,4,4,4,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,
+    1,4,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,
+    1,4,4,4,4,4,4,4,4,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,
+    1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1]
+};
+
+// bad index function arughh this code is duct tape  and paper
 
 fn main() {
     println!("Hello, world!");
@@ -17,7 +57,9 @@ fn main() {
     let video_subsystem = sdl_context.video().unwrap();
 
     // i think this is the window we will be outputing to
-    let window = video_subsystem.window("wolfenstein-style-renderer", SCREEN_WIDTH, SCREEN_HEIGHT)
+    let window = video_subsystem.window("wolfenstein-style-renderer",
+                                                SCREEN_WIDTH.try_into().unwrap(), 
+                                                SCREEN_HEIGHT.try_into().unwrap())
         .position_centered()
         .build()
         .unwrap();
@@ -33,6 +75,15 @@ fn main() {
     // game loop (i suspect we will do most of the code here)
     let mut i = 0;
     let mut event_pump = sdl_context.event_pump().unwrap();
+    // game loop varibles
+    let mut pos: Vec2 = Vec2 {x: 22., y: 12.}; // player posit<i32 as TryInto<u8>>::try_into(m_index(map_x, map_y)).unwrap()ion
+    let mut dir: Vec2 = Vec2 {x: -1., y: 0.}; // player direction
+    let mut plane: Vec2 = Vec2 {x: 0., y: 0.66}; // pos camera plane
+
+    let mut time: f32 = 0.; // time of current frame
+    let mut old_time: f32 = 0.; // time of previous frame
+
+
     'main: loop {
         i = (i +1) % 255;
         canvas.set_draw_color(Color::RGB(i, 64, 255 - i));
@@ -48,6 +99,74 @@ fn main() {
             }
         }
         // where most of the code will go \/\/\/
+        // raycasting code
+        let mut x: i32 = 0;
+        while x < SCREEN_WIDTH {
+            let w: f32 = SCREEN_WIDTH as f32;
+            let camera_x: f32 = (2 * x as i32 / SCREEN_WIDTH - 1) as f32;
+            let ray_dir = Vec2{x: dir.x + plane.x * camera_x,
+                                     y: dir.y + plane.y * camera_x};
+            // the positon of us i supppose
+            let mut map_x: i32 = pos.x as i32;
+            let mut map_y: i32 = pos.y as i32;
+            
+            // the length of ray from current positon to next x or y-side
+            let mut side_dist: Vec2;
+            
+            // urm idk goto line 98 in raycaster_flat.cpp
+            let delta_dist: Vec2 = Vec2 {
+                x: ((ray_dir.x * ray_dir.x) + 1.).sqrt() / (ray_dir.x * ray_dir.x),
+                y: ((ray_dir.y * ray_dir.y) + 1.).sqrt() / (ray_dir.y * ray_dir.y)
+            };
+            
+            let perp_wall_dist: f32;
+            
+            // step direction
+            let step_x: i32;
+            let step_y: i32;
+            
+            let mut hit: i32 = 0;
+            let mut side: i32;
+            // calculate step and initial side Dist note that since we calate x first we set y as 0
+            if ray_dir.x < 0. {
+                step_x = -1;
+                side_dist = Vec2 {x: (pos.x - (map_x as f32)) * delta_dist.x, y: 0.};
+            } else {
+                step_x = 1;
+                side_dist = Vec2 {x: ((map_x as f32) + 1. - pos.x) * delta_dist.x, y: 0.}
+            }
+            if ray_dir.y < 0. {
+                step_y = -1;
+                side_dist.y = (pos.y - map_y as f32) * delta_dist.y
+            } else {
+                step_y = 1;
+                side_dist.y = (map_y as f32 + 1. - pos.y) * delta_dist.y
+            }
+
+            // DDA !!!!
+            while hit == 0 
+            {
+                if side_dist.x < side_dist.y
+                {
+                    side_dist.x += delta_dist.x;
+                    map_x += step_x;
+                    side = 0;
+                } 
+                else 
+                {
+                    side_dist.y += delta_dist.y;
+                    map_y += step_y;
+                    side = 0;
+                }
+                
+                let index: usize = ((map_y * MAP_WIDTH as i32) + map_x) as usize;
+                println!("map_y: {}\nscreen_width: {}\nmap_x: {}", map_y, SCREEN_WIDTH, map_x);
+                if WORLD_MAP.grid[index] > 0 {
+                    hit = 1;
+                }
+            }
+            x += 1;
+        }
 
         canvas.present();
         ::std::thread::sleep(Duration::new(0, 1_000_000_000u32 / 60));
